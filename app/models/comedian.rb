4939f7ce4322604020f73b9d378b540d16c3365c
abarnes26 <abarnes26@yahoo.com>
2017-12-01 11:00:42 -0700

    unable to link the two databases together, final commit

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,10 @@
+
 class Comedian < ActiveRecord::Base
+  has_many :specials
+
+  def self.specials
+    specials = Special.where(comedian_id: "#{self.id}")
+    specials.update(name: self.name, age: self.age)
+  end
 
 end
